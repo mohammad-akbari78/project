50b508eb217a8919c3837a79f99143db84bcafe9
mohammad <makbari78.tab@gmail.com>
2024-06-23 11:20:00 +0330

    change txt file

--- A.pas
+++ A.pas
@@ -1,14 +1,18 @@
 var
-    x:integer;
-    y:integer;
-begin
+ x:integer;
+ y:integer;
 
+begin
  y:=8;
 for x:=1 to 8 do
 begin
 for y:=1 to x do
 begin
-    writeln(x);
-   end;
-   end;
+writeln(y);
+
+
+end;
+writeln("#");
+end;
 end.
+
